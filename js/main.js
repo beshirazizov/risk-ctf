b3fc4ad057235f5d8c94f892d92fa019e05e1368
Bashir Azizov <bashirja@code.edu.az>
2022-09-20 00:45:13 +0400

    asdads

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-AOS.init({once: true});
+AOS.init({ once: true });
 
 var countDownDate = new Date("Nov 8, 2022 00:00:00").getTime();
 
@@ -7,7 +7,7 @@ var countdown = setInterval(function () {
   var distance = countDownDate - now;
   if (distance < 0) {
     clearInterval(countdown);
-    $("#countdown-wrapper").addClass("d-none")
+    $("#countdown-wrapper").addClass("d-none");
   } else {
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor(
@@ -16,6 +16,126 @@ var countdown = setInterval(function () {
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    $("#remaining-time").html(days + " gün " + hours + " saat " + minutes + " dəqiqə " + seconds + " saniyə")
+    $("#remaining-time").html(
+      days +
+        " gün " +
+        hours +
+        " saat " +
+        minutes +
+        " dəqiqə " +
+        seconds +
+        " saniyə"
+    );
   }
 }, 1000);
+
+particlesJS("particles", {
+  particles: {
+    number: {
+      value: 120,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+    color: {
+      value: "#0d6efd",
+    },
+    shape: {
+      type: "circle",
+      stroke: {
+        width: 0,
+        color: "#000000",
+      },
+      polygon: {
+        nb_sides: 5,
+      },
+      image: {
+        src: "img/github.svg",
+        width: 100,
+        height: 100,
+      },
+    },
+    opacity: {
+      value: 0.5,
+      random: false,
+      anim: {
+        enable: false,
+        speed: 1,
+        opacity_min: 0.1,
+        sync: false,
+      },
+    },
+    size: {
+      value: 3,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 40,
+        size_min: 0.1,
+        sync: false,
+      },
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#0d6efd",
+      opacity: 0.4,
+      width: 1,
+    },
+    move: {
+      enable: true,
+      speed: 6,
+      direction: "none",
+      random: false,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 1200,
+      },
+    },
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: true,
+        mode: "repulse",
+      },
+      onclick: {
+        enable: true,
+        mode: "push",
+      },
+      resize: true,
+    },
+    modes: {
+      grab: {
+        distance: 400,
+        line_linked: {
+          opacity: 1,
+        },
+      },
+      bubble: {
+        distance: 400,
+        size: 40,
+        duration: 2,
+        opacity: 8,
+        speed: 3,
+      },
+      repulse: {
+        distance: 200,
+        duration: 0.4,
+      },
+      push: {
+        particles_nb: 4,
+      },
+      remove: {
+        particles_nb: 2,
+      },
+    },
+  },
+  retina_detect: true,
+});
